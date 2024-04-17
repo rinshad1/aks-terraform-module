7cfcbe180709676f128e39f74343b6703a2a3a0a
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2024-04-17 10:54:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name = "backend4rinshad"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    client_id       = "9e0cf4d7-dad9-4bf5-8427-74c4c1a7cc39"
+    client_secret   = ${{ secrets.client_secret }}
+    tenant_id       = "61b2db07-fac4-4636-8c07-ab264dade2fa"
+    subscription_id = "a1959dd9-b11d-4f5c-aa2e-e85c59dbe727"
   }
 }
