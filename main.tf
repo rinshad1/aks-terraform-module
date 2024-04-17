80095b58a41e484be37cf1780a05338e63090e6e
rinshad1 <103164137+rinshad1@users.noreply.github.com>
2024-04-17 12:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
   features {
   }
+  client_id       = "9e0cf4d7-dad9-4bf5-8427-74c4c1a7cc39"
+  client_secret   = "pVa8Q~-H1-0sNp1F478XVgLqRs5.WB33fkTWYa83"
+  tenant_id       = "61b2db07-fac4-4636-8c07-ab264dade2fa"
+  subscription_id = "a1959dd9-b11d-4f5c-aa2e-e85c59dbe727"
 }
 
 resource "azurerm_resource_group" "rg1" {
